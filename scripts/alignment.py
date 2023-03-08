2bce56004fa8f8d51cc03cfdfbafe9d6a2f31f1f
keenanmanpearl <keenan.manpearl@gmail.com>
2023-03-07 22:18:37 -0700

    changed alignment functions

--- scripts/alignment.py
+++ scripts/alignment.py
@@ -41,10 +41,10 @@ def compare_sequences(query, sequence, match_score, gap_score, mismatch_score, t
     for i in range(1, len(query)+1):
         for j in range(1, len(sequence)+1):
             scores[i][j] = max(
-                scores[i][j-1] + gap_score,
-                scores[i-1][j] + gap_score,
+                0,
                 scores[i-1][j-1] + score(query[i-1], sequence[j-1], match_score, mismatch_score),
-                0
+                scores[i-1][j] + gap_score,
+                scores[i][j-1] + gap_score,
             )
             if scores[i][j] >= best_score: 
                 best_score = scores[i][j] 
