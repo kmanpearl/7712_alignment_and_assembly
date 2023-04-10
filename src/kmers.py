2cf09a792e6787a8ff20ba7aef4e1c58ee75871c
keenanmanpearl <keenan.manpearl@gmail.com>
2023-04-09 22:11:19 -0600

    added more docstrings

--- src/kmers.py
+++ src/kmers.py
@@ -7,6 +7,10 @@ Classes and functions needed to create kmers
 
 
 class Kmer:
+    """
+    classes to hold information about kmers
+    """
+
     def __init__(self, read_id, id, sequence, prefix, suffix, start, stop, direction):
         self.read_id = read_id
         self.id = id
@@ -17,23 +21,19 @@ class Kmer:
         self.stop = stop
         self.direction = direction
 
-    # def __eq__(self, other):
-    #    return self.sequence == other.sequence
-
 
 def create_reads_kmers(read_dict, k, id, direction):
     """
-     Parameters
-    ----------
-    k : INT
-        size of kmer
-    read_dict : DICT
-        read_id (keys) and sequence (values)
+    create kmers for reads
 
-    Returns
-    -------
-    kmers : LIST
-        list of Kmer objects
+    Args:
+        read_dict (dict): read ids (keys) and sequences (values)
+        k (int): length of kmers
+        id (int): number to use as starting id for first kmer (0 for forward direction)
+        direction (int): 1 for forward or -1 for reverse
+
+    Returns:
+        list : instances of class Kmer made from reads, and id to be used for start id for reverse kmers
     """
     kmers = []
     for read_id, read in read_dict.items():
@@ -50,6 +50,16 @@ def create_reads_kmers(read_dict, k, id, direction):
 
 
 def create_query_kmers(query, k):
+    """
+    create kmers from query sequence
+
+    Args:
+        query (str): sequence of query
+        k (int): size of kmer
+
+    Returns:
+        list: instances of class Kmer that make up query sequence
+    """
     kmers = []
     id = 0
     for i in range(len(query) - k + 1):
@@ -64,20 +74,34 @@ def create_query_kmers(query, k):
     return kmers
 
 
-"""
-def get_kmers(read_dict, k):
-    kmers = create_reads_kmers(k, read_dict)
-    return kmers
-"""
-
-
 def get_all_kmers(read_dict, rvs_read_dict, k):
+    """
+    generate forward and reverse read kmers
+
+    Args:
+        read_dict (dict): forward reads with read ids (key) and sequences (value)
+        rvs_read_dict (dict): reverse reads with read ids (key) and sequences (value)
+        k (int): size of kmers
+
+    Returns:
+        list : instances of class Kmers that make up all forward and reverse reads
+    """
     fwd_kmers, id = create_reads_kmers(read_dict, k, 0, 1)
     rvs_kmers, id = create_reads_kmers(rvs_read_dict, k, id, -1)
     return fwd_kmers + rvs_kmers
 
 
 def create_contig_kmers(contigs, k):
+    """
+    create kmers of contigs
+
+    Args:
+        contigs (list): instances of class Contigs assembled from reads
+        k (int): size of kmers
+
+    Returns:
+        list: instances of class Kmer assembled from all contigs
+    """
     kmers = []
     id = 0
     for contig in contigs:
